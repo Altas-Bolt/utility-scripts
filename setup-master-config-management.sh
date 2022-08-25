8272390aa3e6af328e0911082035139ca2f8a2cc
Raghav Sharma <raghav_tinker@yahoo.com>
2022-08-25 15:15:39 +0530

    master setup

--- setup-master-config-management.sh
+++ setup-master-config-management.sh
@@ -1,7 +1,14 @@
-# copy files from the state folder
-
 # create file server folder
 mkdir -p /srv/salt
 
 # copy all state files to the file server
-cp -r state/* /srv/salt/
+cp -r state/* /srv/salt
+
+# copy remade config file
+cp config/master /etc/salt/master
+
+# restart salt-master service
+service salt-master restart
+
+# win-repo get
+salt-run winrepo.update_git_repos
